d9f3a077b6c871e11e76fbb16cf6a0f3feecfc28
ivankossdev <ivan.koss@yandex.ru>
2022-05-20 13:21:47 +0300

    Добавлен поиск камер по IP Добавлена  функция очистки ARP таблицы

--- main.py
+++ main.py
@@ -40,17 +40,33 @@ def get_ping_result(a, b, c, d):
     try:
         subprocess.run(cmd_str, creationflags=DETACHED_PROCESS, check=True)
     except subprocess.CalledProcessError as err:
-        return f"Not {a}.{b}.{c}.{d}"
+        return False
     else:
-        return f"Searched {a}.{b}.{c}.{d}"
+        return f"{a}.{b}.{c}.{d}"
+
+
+def search_ip_address(first, last):
+    ip_addreses = []
+    for camera_ip in range(first, last+1):
+        if get_ping_result(192, 168, 15, camera_ip):
+            ip_addreses.append(get_ping_result(192, 168, 15, camera_ip))
+    return ip_addreses
+
+
+def clear_arp_table():
+    subprocess.run("netsh interface ip delete arpcache", creationflags=0x00000008, check=True)
 
 
 if __name__ == '__main__':
-    print("Сетевые адреса")
-    addr = netifaces.ifaddresses(interfaces()[0])
-    for x in addr[netifaces.AF_INET]:
-        print(x)
+    # print("Сетевые адреса")
+    # addr = netifaces.ifaddresses(interfaces()[0])
+    # for x in addr[netifaces.AF_INET]:
+    #     print(x)
     print("Камеры")
-    for camera_ip in range(50, 54):
-        print(get_ping_result(192, 168, 15, camera_ip))
+
+    print(search_ip_address(50, 53))
+    clear_arp_table()
+
+
+
 
